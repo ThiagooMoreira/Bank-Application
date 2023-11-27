000b134b3122805e53e03c8efb1e83195e2c8fb7
ThiagooMoreira <112581320+ThiagooMoreira@users.noreply.github.com>
2023-11-27 16:47:59 +0000

    Add files via upload

---
+++ DAL/DAO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Sql;
+
+
+namespace DAL
+{
+    //DAL layer or Data Access Layer.
+    //DAO to provide connection to stored procedures/communicates with the database
+    public class DAO
+    {
+      
+        SqlConnection con;
+        public DAO()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCon"].ConnectionString);
+        }
+
+
+
+        public SqlConnection OpenCon()
+        {
+            if (con.State == ConnectionState.Broken || con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+        public void CloseCon()
+        {
+            if (con != null)
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
+
---
+++ DAL/NewAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace DAL
+{
+    
+    public class NewAccount:DAO
+    {
+        //Method to Add account. Also uses stored procedure
+        public void AddAccount(string fn, string sn, string email, string pnumber, string address1, string address2, string city, string county, string acctype, int initbal, int overd)
+        {
+            SqlCommand cmd = OpenCon().CreateCommand();
+            cmd.CommandText = "uspAddAccount";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fn", fn);
+            cmd.Parameters.AddWithValue("@sn", sn);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@pnumber", pnumber);
+            cmd.Parameters.AddWithValue("@address1", address1);
+            cmd.Parameters.AddWithValue("@address2", address2);
+            cmd.Parameters.AddWithValue("@city", city);
+            cmd.Parameters.AddWithValue("@county", county);
+            cmd.Parameters.AddWithValue("@acctype", acctype);
+            cmd.Parameters.AddWithValue("@initbal", initbal);
+            cmd.Parameters.AddWithValue("@overd", overd);
+            cmd.ExecuteNonQuery();
+            CloseCon();
+        }
+
+
+    }
+}
