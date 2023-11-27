c3c1bcca2453bf1c68cebca35c9524d6ecfe15cf
ThiagooMoreira <112581320+ThiagooMoreira@users.noreply.github.com>
2023-11-27 16:48:36 +0000

    Add files via upload

---
+++ BIZ/CurrentAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIZ
+{
+    //BIZ Layer or Business Layer.
+    public class CurrentAccount:User
+    {
+        public decimal OverDraftLimit { get; set; }
+
+        //Gets values from class base User and also adds overdraft limit == overd.
+        public CurrentAccount(string fn, string sn, string email, string pnumber, string address1, string address2, string city, string county, string acctype, int accnumber, int sortcode, int initbal, decimal overd)
+        : base(fn, sn, email, pnumber, address1, address2, city, county, acctype, accnumber, sortcode, initbal)
+        {
+            OverDraftLimit = overd;
+        }
+    }
+}
---
+++ BIZ/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIZ
+{
+   public class User
+    {
+        //Class base User with all the informations
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string AccountType { get; set; }
+        public int AccountNumber { get; set; }
+        public int SortCode { get; }
+        public int InitialBalance { get; set; }
+
+
+        public User(string fn, string sn, string email, string pnumber, string address1, string address2, string city, string county, string acctype, int accnumber, int sortcode, int initbal)
+        {
+            Firstname = fn;
+            Surname = sn;
+            Email = email;
+            PhoneNumber = pnumber;
+            AddressLine1 = address1;
+            AddressLine2 = address2;
+            City = city;
+            County = county;
+            AccountType = acctype;
+            AccountNumber = accnumber;
+            SortCode = sortcode;
+            InitialBalance = initbal;
+        }
+    }
+}
